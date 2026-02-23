e307c0b78f9699725ed373c75a45b468a525a348
Jacobo Ruiz <jaruizca@unirioja.es>
2026-02-23 20:50:31 +0100

    Crear clases App, DBStub, MailerStub, Repositorio, Servicio, ToDo

---
+++ src/main/java/com/tt1/test/MailerStub.java
@@ -0,0 +1,4 @@
+package com.tt1.test;
+
+public class MailerStub {
+}
---
+++ src/main/java/com/tt1/test/ToDo.java
@@ -0,0 +1,37 @@
+package com.tt1.test;
+
+import java.util.Date;
+
+public class ToDo {
+    public void setId(Integer id) {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+
+    public Integer getId() {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+
+    public void setNombre(String nombre) {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+
+    public void setDescripcion(String descripcion) {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+
+    public String getDescripcion() {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+
+    public void setFechaLimite(Date fechaLimite) {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+
+    public Date getFechaLimite() {
+        throw new UnsupportedOperationException("Clase no implementada aún");
+    }
+}

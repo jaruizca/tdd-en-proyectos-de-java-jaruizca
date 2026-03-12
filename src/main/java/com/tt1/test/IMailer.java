e66bd433aecd076d40c5025b08d677bec3b6b396
Jacobo <jaruizca@unirioja.es>
2026-03-12 19:29:02 +0100

    Eliminar uso de paramref en javadoc

--- src/main/java/com/tt1/test/IMailer.java
+++ src/main/java/com/tt1/test/IMailer.java
@@ -5,7 +5,7 @@ public interface IMailer {
     // correctamente y falso si ha ocurrido algún fallo.
 
     /**
-     * Envía un email con el contenido {@paramref mensaje} al destinatario {@paramref direccion}
+     * Envía un email con el contenido {@code mensaje} al destinatario {@code direccion}
      * @param direccion dirección del destinatario del correo electrónico.
      * @param mensaje mensaje que se quiere envíar al destinatario.
      * @return Devuelve true si el email se ha envíado
